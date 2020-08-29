2b651bb959bbb934b575e5474ffec04a233219de
xpopelas <janopopelas@gmail.com>
2020-08-29 17:43:29 +0200

    add basic crawl from json files

--- msg-analysis/loading.py
+++ msg-analysis/loading.py
@@ -1,4 +1,5 @@
 from models import *
+import os
 
 
 def create_metadata(entries=None):
@@ -11,3 +12,17 @@ def create_metadata(entries=None):
         for __entry in entries:
             __metadata.from_entry(__entry)
     return __metadata
+
+
+def find_all_json(start_directory):
+    """
+    :param start_directory: string, path to directory, from which the search should begin
+    :return: [string], list of all paths containing json files
+    """
+    result = []
+    for dir_name, subdir_list, file_list in os.walk(start_directory):
+        for file in file_list:
+            file = file.strip()
+            if file.endswith(".json"):
+                result.append(dir_name + '/' + file)
+    return result
